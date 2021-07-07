119ad04cfeb375cb2d9b11a506ea9618e3f1b7ac
Simran Panthi <simranpanthi101@gmail.com>
2021-07-07 16:36:09 +0545

    Performed redis search using python client

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from redisearch import Client, TextField, IndexDefinition
+
+# Creating a client with a given index name
+client = Client("index-document")
+
+# IndexDefinition is avaliable for RediSearch 2.0+
+definition = IndexDefinition(prefix='doc:')
+
+# Creating the index definition and schema
+client.create_index((TextField("title"), TextField("body")), definition=definition)
+
+folder = "/home/simran/projects/redisearch/documents/"
+
+for file in os.listdir(folder):    
+    filepath = os.path.join(folder, file)
+    f = open(filepath, 'r')
+    client.redis.hset('doc:'+file,
+                mapping={
+                    'title': file,
+                    'body': f.read()
+                })
+
+    f.close()
+
+# Simple search
+res = client.search("document")
+
+# the result has the total number of results, and a list of documents
+print(res.total) 
+print(res.docs) 
